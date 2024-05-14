699624921c8c020d6ba0513eeda4b76012a105d7
Mohsin A. <hmdmohsinali@gmail.com>
2024-05-14 11:38:50 +0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import path from 'path'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/connectToMOngoDB.js'
@@ -11,6 +12,7 @@ import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
 
 dotenv.config()
 app.use(express.json()) // from req.body to parse incoming req from json payload
@@ -19,6 +21,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", 'index.html'))
+})
 // app.get('/', (req, res)=>{
 //     res.send("hello mohsin to the backend")
 // })
